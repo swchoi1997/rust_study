3a473e379cab3d4ec75b758c9c071e6dfd8bd48f
SangWon <swchoi1997@naver.com>
2024-08-14 00:59:05 +0900

    struct2

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,97 @@
+#![allow(unused_variables)]
+
+static mut ERROR: i32 = 0;
 
 #[derive(Debug)]
-struct Rectangle{
-    width   : u32,
-    height  : u32,
+struct File {
+    name: String,
+    data: Vec<u8>,
 }
-impl Rectangle {
-    fn square(size: u32) -> Self {
-        Self{
-            width: size,
-            height: size
+
+impl File{
+    fn new(name: &str) -> File{
+        File{
+            name: String::from(name),
+            data: Vec::new()
         }
     }
-    fn area(&self) -> u32 {
-        return self.height * self.width;
+
+    fn new_with_data(name: &str, data: &Vec<u8>) -> File{
+        File{
+            name: String::from(name),
+            data: data.clone()
+        }
     }
 
-    fn half_size(&self) -> u32{
-        return self.area() / 2;
+    fn read(&mut self, save_to: &mut Vec<u8>) -> usize {
+        self.data.append(save_to);
 
+        self.data.len()
     }
+}
+
+fn open(f: &mut File) -> bool{
+    true
+}
+
+fn close(f: &mut File) -> bool {
+    true
+}
+
+fn read(f: &mut File, save_to: &mut Vec<u8>) -> usize {
+    let mut tmp:Vec<u8> = f.data.clone();
+    let tmp_size = tmp.len();
+
+    save_to.reserve(tmp_size);
+    save_to.append(&mut tmp);
+
+    tmp_size
 }
 
 fn main() {
-    let rect: Rectangle = Rectangle{
-        width: 32,
-        height: 2,
+    let mut f2 = File {
+        name: String::from("t2.txt"),
+        data: vec![114, 117, 115, 116, 33],
     };
 
-    let size: u32 = rect.area();
-    let half_size: u32 = rect.half_size();
 
-    dbg!(size);
-    dbg!(half_size);
+    let mut buffer: Vec<u8> = vec![];
 
-    let square: Rectangle = Rectangle::square(10);
+    if open(&mut f2){
+        let read_size: usize = read(&mut f2, &mut buffer);
 
-    dbg!(&square);
+        close(&mut f2);
+    }
+
+    let text = String::from_utf8_lossy(&buffer);
+
+    println!("{:?}", f2);
+    println!("{:?}", text);
+
+
+    let mut f3: File = File::new("t3.txt");
+
+    println!("{:?}, {:?}", &f3.name, &f3.data);
+
+    open(&mut f3);
+    unsafe {
+        if ERROR != 0 {
+            panic!("When Read File Occured Error")
+        }
+    }
+    let size: usize = f3.read(&mut vec![1, 2, 3, 4, 5]);
+    close(&mut f3);
+    unsafe {
+        if ERROR != 0 {
+            panic!("When Read File Occured Error")
+        }
+    }
+
+    println!("{size}")
+
+
+
+
+
 
 }
